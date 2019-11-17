33fd14cc2fc8eef05ce28bc8aa9b87e21e989fa4
adam-mata <40769841+adam-mata@users.noreply.github.com>
2019-11-17 18:49:42 +0100

    Needed libraries

--- PredictBus.R
+++ PredictBus.R
@@ -1,1 +1,10 @@
-// TODO: wczytywanie
+# isntall libraries
+install.packages('plyr') # SQL
+install.packages('dplyr') # SQL
+install.packages("lubridate") # Dates operations
+
+# load libraries
+library(plyr)
+library(dplyr)
+library(lubridate)
+
