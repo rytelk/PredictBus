ed2491b911ae5e7469eb3de7036b577fe6f39f24
nc-gcz <gcz@netcompany.com>
2019-12-03 22:35:57 +0100

    Add Adam's changes

--- BusesRepository.R
+++ BusesRepository.R
@@ -1,14 +1,14 @@
 source(paste(path,"TimeUtils.R",sep=''))
 
-GetBuses <- function(bus_stop)
+GetBuses <- function(db_path, bus_stop)
 {
   library(RSQLite)
   library(stringr)
   
-  conn <- dbConnect(RSQLite::SQLite(), "Buses.db")
+  db_filename <- paste(path, "Buses.db", sep = '')
+  conn <- dbConnect(RSQLite::SQLite(), db_filename)
   
   sqlQuery <- str_interp("select lineString, cast(substr(timeDatetime, 12, 2) as integer) as hour, delayLet as delay, (case when cast(substr(timeDatetime, 9, 2) as integer) IN (26, 27) then 1 else 0 end) as isWeekend from Buses where nextStopString = '${bus_stop}';")
-  
   buses <- dbGetQuery(conn, sqlQuery)
   dbDisconnect(conn)
   
--- MAIN.R
+++ MAIN.R
@@ -10,12 +10,32 @@ path_mataa = "C:\\Users\\amata\\Desktop\\PLIKI_PPD\\PPD_DATES\\"
 path_czarnockig = "C:\\Development\\_university\\PredictBus\\"
 path_rytelk = "/home/krystian/Documents/PredictBus/"
 
-path = path_rytelk # change this
+path = path_czarnockig # change this
 source(paste(path,"BusesRepository.R",sep=''))
 
-buses <- GetBuses('3027-Dolna')
+bus_stop = '3027-Dolna'
+buses <- GetBuses(path, bus_stop)
 
-cart.tree <- rpart(delay ~ lineString + hour + isWeekend, data = buses)
+paramLineString = "501";
+current_time = "2019-12-01 14:34:25"
+current_time_posix <- as.POSIXct(strptime(current_time, "%Y-%m-%d %H:%M:%S"))
+
+current_time_hour <- as.integer(hour(current_time_posix))
+current_time_dw <- weekdays(current_time_posix)
+
+if(current_time_dw == "Saturday" || current_time_dw == "Sunday")
+{
+  current_time_is_weekend <- 1  
+} else {
+  current_time_is_weekend <- 0
+}
+
+df_test <- data.frame(paramLineString, current_time_hour, current_time_is_weekend)
+colnames(df_test) <- c("lineString", "hour", "isWeekend")
+
+# predykcja
+cart.tree.pred <- predict(cart.tree, df_test)
 summary(cart.tree)
 
-
+output <- as.double(cart.tree.pred)
+cat("Opóźnienie dla linii ", paramLineString, ", czas: ", current_time, " = ", output, " sekund", sep = '')
