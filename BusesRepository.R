87514693d66e51205589b9ea037de755df36932b
Kris <krystian.rytel@gmail.com>
2019-12-03 21:33:04 +0100

    Sql query

--- BusesRepository.R
+++ BusesRepository.R
@@ -7,7 +7,7 @@ GetBuses <- function(bus_stop)
   
   conn <- dbConnect(RSQLite::SQLite(), "Buses.db")
   
-  sqlQuery <- str_interp('select lineString, cast(substr(timeDatetime, 12, 2) as integer) as timeDatetime, delayLet as delay from Buses where nextStopString = "${bus_stop}";')
+  sqlQuery <- str_interp("select lineString, cast(substr(timeDatetime, 12, 2) as integer) as hour, delayLet as delay, (case when cast(substr(timeDatetime, 9, 2) as integer) IN (26, 27) then 1 else 0 end) as isWeekend from Buses where nextStopString = '${bus_stop}';")
   
   buses <- dbGetQuery(conn, sqlQuery)
   dbDisconnect(conn)
