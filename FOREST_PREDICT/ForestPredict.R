df806fb7372d3e3a42c2611ad71ef6211b1348a3
adam-mata <40769841+adam-mata@users.noreply.github.com>
2019-12-23 16:30:00 +0100

    Random Forest prediction

---
+++ FOREST_PREDICT/ForestPredict.R
@@ -0,0 +1,60 @@
+# --------------------------------------------------------------------------------
+# Parameters of prediction
+# TODO - get full frame of input data
+GLOBAL_bus_stop_id <- '3027'
+GLOBAL_bus_line <- 'N81'
+GLOBAL_query_datetime <- '2018-05-21 00:04:41'
+
+input_df <- data.frame(GLOBAL_bus_stop_id, GLOBAL_bus_line, GLOBAL_query_datetime, stringsAsFactors = FALSE)
+colnames(input_df) <- c('bus_stop_id', 'bus_line', 'query_datetime')
+# --------------------------------------------------------------------------------
+# load libraries
+library(plyr)
+library(dplyr)
+library(rpart)
+library(lubridate)
+library(sqldf)
+library(stringr)
+library(readtext)
+library(RSQLite)
+library(caret)
+# --------------------------------------------------------------------------------
+# set main path
+path_czarnockig = "C:\\Development\\_university\\PredictBus\\"
+path_rytelk = "/home/krystian/Documents/PredictBus/"
+path_mataa = "C:/Users/amata/Desktop/FOREST_PREDICT/"
+
+path = path_mataa
+# --------------------------------------------------------------------------------
+# function to get sql from file
+source(paste(path, 'ForestPredictLib.R', sep = ''))
+# --------------------------------------------------------------------------------
+# open connection
+db_filename <- paste(path, "Buses.db", sep = '')
+connection <- dbConnect(RSQLite::SQLite(), db_filename)
+# --------------------------------------------------------------------------------
+# MAKE FOREST PREDICTION - TODO
+# perform prediction
+main_output_df <- data.frame(
+                             real_delay <- numeric(),
+                             schedule_arrival_time <- factor(),
+                             predicted_delay <- numeric(),
+                             predict_percentage_error <- numeric(),
+                             predict_error <- numeric()
+                            )
+
+for(current_row in 1:nrow(input_df))
+{
+  output_df <- makeForestPrediction(
+                              path = path,
+                              connection = connection,
+                              INPUT_bus_stop_id = input_df[current_row, "bus_stop_id"],
+                              INPUT_bus_line = input_df[current_row, "bus_line"],
+                              INPUT_query_datetime = input_df[current_row, "query_datetime"]
+                             )
+  main_output_df <- rbind(main_output_df, output_df)
+}
+# --------------------------------------------------------------------------------
+# close the connection
+dbDisconnect(connection)
+# --------------------------------------------------------------------------------
---
+++ FOREST_PREDICT/ForestPredictLib.R
@@ -0,0 +1,78 @@
+getSQL <- function(filepath)
+{
+  con = file(filepath, "r")
+  sql.string <- ""
+  
+  while (TRUE)
+  {
+    line <- readLines(con, n = 1)
+    
+    if ( length(line) == 0 )
+    {
+      break
+    }
+    
+    line <- gsub("\\t", " ", line)
+    
+    if(grepl("--",line) == TRUE)
+    {
+      line <- paste(sub("--","/*",line),"*/")
+    }
+    
+    sql.string <- paste(sql.string, line)
+  }
+  
+  close(con)
+  return(sql.string)
+}
+
+makeForestPrediction <- function(path, connection, INPUT_bus_stop_id, INPUT_bus_line, INPUT_query_datetime)
+{
+    # get learn data
+    forest_sql_file_path <- paste(path, "ForestPredictForR.sql", sep = '')
+    forest_sql_code <- getSQL(forest_sql_file_path)
+    forest_sql_code <- str_replace_all(forest_sql_code, '>>BUS_STOP_ID<<', INPUT_bus_stop_id)
+    forest_buses_data <- dbGetQuery(connection, forest_sql_code)
+    # --------------------------------------------------------------------------------
+    # build predict model using random forest
+    train.control.cross_validation <- trainControl(method = 'cv', number = 10)
+    model_rf <- train(delay ~ lineString + isWeekend + hour, data = forest_buses_data, method = 'rf', trControl = train.control.cross_validation)
+    # --------------------------------------------------------------------------------
+    # prepare test data frame
+
+    EXTRACTED_POSIX_query_datetime <- as.POSIXct(strptime(INPUT_query_datetime, "%Y-%m-%d %H:%M:%S"))
+    EXTRACTED_hour <- as.integer(hour(EXTRACTED_POSIX_query_datetime))
+    EXTRACTED_day_of_week <- weekdays(EXTRACTED_POSIX_query_datetime)
+
+    if( EXTRACTED_day_of_week == "Saturday" || EXTRACTED_day_of_week == "Sunday" )
+    {
+      EXTACTED_is_weekend <- 1
+    } else {
+      EXTACTED_is_weekend <- 0
+    }
+
+    df_test = data.frame(INPUT_bus_line, EXTACTED_is_weekend, EXTRACTED_hour)
+    colnames(df_test) <- c("lineString", "hour", "isWeekend")
+    # --------------------------------------------------------------------------------
+    forest_prediction <- predict(model_rf, df_test)
+    predicted_delay <- as.double(forest_prediction)
+    # --------------------------------------------------------------------------------
+    # compute error
+    error_sql_code_path <- paste(path, "GetClosestRecordForR.sql", sep = '')
+    error_sql_code <- getSQL(error_sql_code_path)
+    error_sql_code <- str_replace_all(error_sql_code, '>>BUS_STOP_ID<<', INPUT_bus_stop_id)
+    error_sql_code <- str_replace_all(error_sql_code, '>>BUS_LINE<<', INPUT_bus_line)
+    error_sql_code <- str_replace_all(error_sql_code, '>>QUERY_DATETIME<<', INPUT_query_datetime)
+
+    side_buses_data <- dbGetQuery(connection, error_sql_code)
+    real_world_delay <- mean(side_buses_data$delay)
+    real_world_arrival_datetime <- side_buses_data$timetable_datetime[1]
+    percentage_error <- abs(predicted_delay - real_world_delay) / abs(real_world_delay)
+    # predict_error = real_world_delay - predicted_delay
+    predict_error <- real_world_delay - predicted_delay
+    
+    df <- data.frame(real_world_delay, real_world_arrival_datetime, predicted_delay, percentage_error, predict_error)
+    colnames(df) <- c('real_delay', 'schedule_arrival_time', 'predicted_delay', 'predict_percentage_error', 'predict_error')
+    
+    return(df)
+}
