38a3b465eea508d69322c1fe0f009efccb543fcf
nc-gcz <gcz@netcompany.com>
2020-01-27 01:59:37 +0100

    Modify gitignore, add script for getting mean + std.dev.

--- commands.sql
+++
@@ -1,4 +0,0 @@
-create index IDX_nextStopString 
-on Buses (nextStopString);
-
-select * from Buses where nextStopString = "3027-Dolna";
--- compute_metrics.R
+++ compute_metrics.R
@@ -3,15 +3,18 @@ path_rytelk = "/home/krystian/Documents/PredictBus/"
 path_mataa = "C:/Users/amata/Desktop/PPD-REPO/STATISTICAL_PREDICT_BUS/"
 
 path = path_czarnockig
-
-data = "FOREST_PREDICT\\results\\buses_weekend_day.csv"
+datapath = "STATISTICAL_PREDICT_BUS\\results\\"
 # --------------------------------------------------------------------------------
-# Parameters of prediction
-input <- read.csv(paste(path, data, sep = ''), sep = ',')
 
-print("Predict_error")
-mean(input$predict_error, na.rm=TRUE)
-sd(input$predict_error, na.rm=TRUE);
-print("Percentage_error`")
-mean(input$predict_percentage_error, na.rm=TRUE)
-sd(input$predict_percentage_error, na.rm=TRUE)
+files <- c('buses_weekend_day.csv', 'buses_weekend_night.csv', 'buses_weekday_day.csv', 'buses_weekday_night.csv')
+for (datafile in files) {
+  # Parameters of prediction
+  print(datafile)
+  input <- read.csv(paste(path, datapath, datafile, sep = ''), sep = ',')
+  
+  print("Predict_error")
+  mean(input$predict_error, na.rm=TRUE)
+  sd(input$predict_error, na.rm=TRUE);
+  print("Percentage_error`")
+  mean(input$predict_percentage_error, na.rm=TRUE)
+}
